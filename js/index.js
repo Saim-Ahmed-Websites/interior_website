f9c64248dea1ce0756d8d3f19c204a206f63a4ae
Saim-Ahmed1 <78466954+Saim-Ahmed1@users.noreply.github.com>
2022-03-03 23:33:04 +0600

    Interior Home

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let burger = document.querySelector('.burger');
+let hMiddle = document.querySelector('.H-middle');
+
+burger.addEventListener("click", () => {
+    hMiddle.classList.toggle('show')
+});
