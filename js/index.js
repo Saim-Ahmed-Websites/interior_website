e8109da143e794f5aa5effb173d026b05c460c8b
Saim-Ahmed1 <78466954+Saim-Ahmed1@users.noreply.github.com>
2022-03-03 23:29:54 +0600

    Interior Home

--- js/index.js
+++
@@ -1,6 +0,0 @@
-let burger = document.querySelector('.burger');
-let hMiddle = document.querySelector('.H-middle');
-
-burger.addEventListener("click", () => {
-    hMiddle.classList.toggle('show')
-});
